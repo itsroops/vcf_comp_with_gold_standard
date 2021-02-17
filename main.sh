0ad71f0aebbb8d4e29e74d741a9b49b7fa517e5e
itsroops <46230392+itsroops@users.noreply.github.com>
2021-02-17 02:44:25 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -123,73 +123,11 @@ read mail_id
 echo -e "\nMail id read......" `date` >> mainlog.txt
 echo -e "\n" >> mainlog.txt
 
-# Navigating to the vcf files
-files=`ls $vcf_folder/*.vcf`
+# Proving executable permissions to the file for all
+chmod a+x generate_plots.sh
 
-# Submitting jobs for all the vcf files
-for var in $files
+# Executing the generation of plots script in the background by taking the variables from the current shell
+source ./generate_plots.sh &
 
-do
-
-# Reading the filename
-file_name=`echo $var | rev | cut -d'/' -f 1 | rev`
-file_name=$out/$file_name
-
-# Declaring array for storing submitted jobs
-declare -a jobs
-
-# Submitting the jobs
-k=`sbatch vcf_comp.sh $python_path $happy_path $vcf_gold $var $bed $file_name $ref --cpus-per-task=$cpt --mem=$mem --time=$tot_time --account=$acc --partition=$par --mail-type=$mail_type --mail-user=$mail_id -x raptor10201`
-
-echo $k......`date` >> mainlog.txt
-
-# Extracting the job ids
-j=$(echo $k | cut -d ' ' -f4)
-jobs+=($j)
-done
-
-echo -e "\nThe 'squeue' command can be used to check the status of the jobs submitted" >> mainlog.txt
-echo -e "\nMoreover, the running status of individual jobs can be found here $curr_path/slurm-jobid.out" >> mainlog.txt
-
-# Running the infinite loop  till all the queued jobs terminate
-l=0
-while [ $l -lt 10 ]
-do
-
-flag=0
-for i in "${jobs[@]}"
-do
-k=`squeue | grep $i`
-len=${#k}
-if [[ $len = 0 ]]; then
-flag=$((flag+1))
-fi
-done
-
-if [[ $flag = ${#jobs[@]} ]]; then
-break
-fi
-
-# Checking the status of the jobs completion every after 2 minutes
-sleep 2m
-
-done
-
-echo -e "\nAll the jobs are complete......" `date` >> mainlog.txt
-
-# Plotting the vcf comparison values
-$curr_path/temp/miniconda3/bin/python3 happy_plots.py $out
-
-echo -e "\nThe bar charts of the comparison are generated in the path $out......" `date` >> mainlog.txt
-
-end_time=$(date +"%s")
-
-# Computing the elapsed seconds
-secs=$((end_time-start_time))
-
-# Dividing seconds into hours, minutes and seconds
-h=$((secs/3600))
-m=$((secs%3600/60))
-s=$((secs%60))
-
-echo -e "\nThe total running time of the program is $h hour(s) $m minute(s) and $s second(s)" >> mainlog.txt
+echo -e "\nYou can monitor the log in the following path:$curr_path/mainlog.txt"
+echo -e "\nThe final plots would be available in the following path:$out"
